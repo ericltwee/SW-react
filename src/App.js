d1efe3d57114c9d814e05e75864cdf2c35269881
Eric Lim T W <59719518+ericltwee@users.noreply.github.com>
2020-11-25 14:18:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./styling/main.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./img/logo.png";
-import Menu from "./components/menu";
 import Monsterr from "./components/Monster";
+
+import Menu from "./components/menu";
 
 function App() {
   return (
@@ -13,8 +14,7 @@ function App() {
         <div className="App-pool">
           <Monsterr />
         </div>
-
-        <Menu />
+        {/* <Menu /> */}
         <img className="App-logo" src={logo} alt="logo" />
       </header>
     </div>
